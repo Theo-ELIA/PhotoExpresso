b19e9030c2fd72330b2ce4081f583cbc773b61bf
Romain <romain@scaleflex.cloud>
2017-02-03 09:52:53 +0100

    debut de l'iOS

---
+++ photoexpresso/photoexpresso/PhotoExpresso/MainMenu.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  PhotoExpresso
+//
+//  Created by Romain on 02/02/17.
+//  Copyright © 2017 Romain. All rights reserved.
+//
+
+import UIKit
+
+class MainMenu: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
+{
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    @IBAction func loadImageButton(_ sender: UIButton) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    private func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = pickedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
